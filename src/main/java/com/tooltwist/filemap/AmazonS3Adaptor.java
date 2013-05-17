1c6d3b7536579949d79e6fc0daaafad2443b7f3a
Ryan Cangco <ryan.cangco@twistresources.com>
2013-05-17 17:09:25 +0800

    updates for s3 adoptor

--- src/main/java/com/tooltwist/filemap/AmazonS3Adaptor.java
+++ src/main/java/com/tooltwist/filemap/AmazonS3Adaptor.java
@@ -1,9 +1,12 @@
 package com.tooltwist.filemap;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.amazonaws.auth.AWSCredentials;
@@ -12,138 +15,196 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.tooltwist.xdata.XDException;
 import com.tooltwist.xdata.XSelector;
 
 public class AmazonS3Adaptor implements IFileGroupAdaptor {
-
-	private AmazonS3 s3;
+	
+	private String baseDir = "/tmp";
+	private AmazonS3Client s3;
 	private String bucketName;
 
 	@Override
 	public void init(XSelector config) throws FilemapException {
 		
+		//check the configuration specified
 		String accessKey = "";
 		String secretKey = "";
 		String region = "";
+		
 		try {
 			bucketName = config.getString("bucketName");
 			accessKey = config.getString("accessKey");
 			secretKey = config.getString("secretKey");
 			region = config.getString("region");
 			
-			if (region == null || "".equals(region))
-				region = Regions.US_WEST_2.getName();
-			
 		} catch (XDException e) {
-			e.printStackTrace();
+			throw new FilemapException("Error parsing config.");
 		}
 		
+		//set s3 credentials 
 		AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 		s3 = new AmazonS3Client(awsCredentials);
 		
-		Region awsRegion = null;
-		for (Regions regions :Regions.values()) {
-			if (regions.getName().equals(region)) {
-				awsRegion = Region.getRegion(regions);
-				break ;
+		//initialize region
+		try {
+			
+			if (region == null || "".equals(region))
+				region = Regions.US_WEST_2.getName();
+			
+			Region awsRegion = null;
+			for (Regions regions :Regions.values()) {
+				if (regions.getName().equals(region)) {
+					awsRegion = Region.getRegion(regions);
+					break ;
+				}
+					
 			}
-				
+			s3.setRegion(awsRegion);
+			
+		} catch (Exception e) {
+			throw new FilemapException("Error setting region.");
 		}
-		s3.setRegion(awsRegion);
 
+	}
+	
+	@Override
+	public boolean exists(String relativePath) throws FileNotFoundException {
+		
+		boolean exists = false;
+		
+		ObjectListing listObjects = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix(relativePath));
+        List<S3ObjectSummary> commonPrefixes = listObjects.getObjectSummaries();
+        if (commonPrefixes.size() > 0)
+        	exists = true;
+		
+		return exists;
 	}
 
 	@Override
-	public OutputStream openForWriting(String relativePath, boolean append) throws IOException {
+	public boolean isDirectory(String relativePath) throws FileNotFoundException {
+		
+		String path = fullPath(relativePath);
+		File file = new File(path);
+		boolean exists = file.isDirectory();
+		return exists;
+	}
+
+	@Override
+	public boolean isFile(String relativePath) throws FileNotFoundException {
+		
+		String path = fullPath(relativePath);
+		File file = new File(path);
+		boolean exists = file.isFile();
+		return exists;
+	}
+
+	@Override
+	public boolean mkdir(String relativePath) throws FileNotFoundException {
+		
+		return true;
+		 
+	}
+
+	@Override
+	public boolean mkdirs(String relativePath) throws FileNotFoundException {
+		
+		return mkdir(relativePath);
+		
+	}
+
+	@Override
+	public AwsPluginOutputStream openForWriting(String relativePath, boolean append) throws IOException {
 			
-		checkBucketIfExistElseCreate();
+		createBucketIfNotExist(bucketName);
 		
-		File asset = new File(relativePath);
-		String assetName = asset.getName();
 		
-		try {
-			PutObjectRequest por = new PutObjectRequest(bucketName, assetName, asset);
-			por.setCannedAcl(CannedAccessControlList.PublicRead);
-			s3.setEndpoint("s3.amazonaws.com");
-			s3.putObject(por);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		AwsPluginOutputStream outputStream = new AwsPluginOutputStream();
+		outputStream.setRelativePath(relativePath);
+		outputStream.setS3(s3);
+		outputStream.setBucketName(bucketName);
 		
-		return null;
+		return outputStream;
+		
 	}
 
 	@Override
 	public InputStream openForReading(String relativePath) throws IOException {
 		
-		checkBucketIfExistElseCreate();
-		
+		createBucketIfNotExist(bucketName);
+        
+        while (relativePath.startsWith("/"))
+        	relativePath = relativePath.substring(1);
+        relativePath = relativePath.replace("//", "/");
         S3Object object = s3.getObject(new GetObjectRequest(bucketName, relativePath));
-        System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
-        S3ObjectInputStream objectContent = object.getObjectContent();
+        
+        return object.getObjectContent();
 
-		return objectContent;
-	}
-
-	@Override
-	public boolean exists(String relativePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isDirectory(String relativePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isFile(String relativePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mkdir(String relativePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mkdirs(String relativePath) throws IOException {
-		// TODO Auto-generated method stub
-		return false;
 	}
 	
 	@Override
+	public Iterable<String> files(String directoryRelativePath) {
+        LinkedList<String> result = new LinkedList<String>();
+        
+        ObjectListing listObjects = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix(directoryRelativePath));
+        List<S3ObjectSummary> commonPrefixes = listObjects.getObjectSummaries();
+        for (S3ObjectSummary s3Obect : commonPrefixes) {
+        	String key = s3Obect.getKey();
+        	
+        	if (directoryRelativePath.endsWith("/"))
+        	key = key.substring(directoryRelativePath.length());
+        	
+        	//remove leading slashes
+        	if (key.startsWith("/"))
+        		key = key.substring(1);	
+        	
+        	//include only if obect is not subfolder.
+        	if (!key.equals("") && !key.endsWith("/"))
+        		result.add(key);
+        	
+        }
+
+        return result;
+    }
+	
+	
+	 /** Delete an object - if they key has any leading slashes, they are removed.
+     */
 	public boolean deleteFile(String relativePath) {
 		
-		checkBucketIfExistElseCreate();
+		if (relativePath.startsWith("/"))
+			relativePath = relativePath.substring(1);
 		
-		File asset = new File(relativePath);
-		String assetName = asset.getName();
-		
-		s3.deleteObject(bucketName, assetName);
+		s3.deleteObject(bucketName, relativePath);
 		
 		return true;
 	}
 
 	@Override
 	public String fileDescription(String relativePath) {
-		return "(Amazon S3: " + relativePath + ")"; // ZZZ Should give details of S3 buckeet, etc.
+		return s3.getResourceUrl(bucketName, relativePath);
+	}
+	
+	private String fullPath(String relativePath) {
+		if (relativePath.startsWith("/"))
+			return baseDir + relativePath;
+		return baseDir + "/" + relativePath;
 	}
 	
 	/**
 	 * Check bucket if already exist, if not create bucket
 	 */
-	private void checkBucketIfExistElseCreate() {
+	private void createBucketIfNotExist(String bucketName) {
 		
 		boolean isBucketExist = false;
 		List<Bucket> listBuckets = s3.listBuckets();
@@ -153,6 +214,7 @@ public class AmazonS3Adaptor implements IFileGroupAdaptor {
 				break;
 			}
 		}
+		
 		
 		if (!isBucketExist)
 			s3.createBucket(bucketName);
