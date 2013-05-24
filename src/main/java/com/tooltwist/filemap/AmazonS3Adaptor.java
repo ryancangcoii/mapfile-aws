59d246a483a8bd59a7f1bcd344ab3197a2de8f26
Ryan Cangco <ryan.cangco@twistresources.com>
2013-05-24 14:13:09 +0800

    remove impletation for isDirectory, defaulted to true

--- src/main/java/com/tooltwist/filemap/AmazonS3Adaptor.java
+++ src/main/java/com/tooltwist/filemap/AmazonS3Adaptor.java
@@ -1,11 +1,8 @@
 package com.tooltwist.filemap;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,17 +10,12 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.tooltwist.xdata.XDException;
 import com.tooltwist.xdata.XSelector;
@@ -94,28 +86,27 @@ public class AmazonS3Adaptor implements IFileGroupAdaptor {
 	@Override
 	public boolean isDirectory(String relativePath) throws FileNotFoundException {
 		
-		boolean isDirectory = false;
+		boolean isDirectory = true;
 		
-        ObjectListing listObjects = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix(relativePath));
-        List<S3ObjectSummary> commonPrefixes = listObjects.getObjectSummaries();
-        for (S3ObjectSummary s3Obect : commonPrefixes) {
-        	String key = s3Obect.getKey();
-        	
-        	if (relativePath.endsWith("/"))
-        	key = key.substring(relativePath.length());
-        	
-        	//remove leading slashes
-        	if (key.startsWith("/"))
-        		key = key.substring(1);	
-        	
-        	//relative path is a directory
-        	if (!key.equals("") && !key.endsWith("/")) {
-        		isDirectory = true;
-        		break;
-        	}
-        		
-        	
-        }
+//		ObjectListing listObjects = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName).withPrefix(relativePath));
+//		List<S3ObjectSummary> commonPrefixes = listObjects.getObjectSummaries();
+//		for (S3ObjectSummary s3Obect : commonPrefixes) {
+//			String key = s3Obect.getKey();
+//
+//			if (relativePath.endsWith("/"))
+//				key = key.substring(relativePath.length());
+//
+//			// remove leading slashes
+//			if (key.startsWith("/"))
+//				key = key.substring(1);
+//
+//			// relative path is a directory
+//			if (!key.equals("") && !key.endsWith("/")) {
+//				isDirectory = true;
+//				break;
+//			}
+//
+//		}
 
         return isDirectory;
 	}
@@ -212,7 +203,7 @@ public class AmazonS3Adaptor implements IFileGroupAdaptor {
 	
 	 /** Delete an object - if they key has any leading slashes, they are removed.
      */
-	public boolean deleteFile(String relativePath) {
+	public boolean delete(String relativePath) {
 		
 		if (relativePath.startsWith("/"))
 			relativePath = relativePath.substring(1);
